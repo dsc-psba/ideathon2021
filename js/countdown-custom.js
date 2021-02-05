8cfbbe6cd30a2a3673a0f438645e6708f884d101
FordanG <60175691+FordanG@users.noreply.github.com>
2021-02-05 16:49:39 +0800

    update page

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2021, 01, 26, 19)}); // year, month, date, hour
+        });
+});		
+
