c7f28bf3e371aa2090cc4d6f7097219a8a465b6f
Timothy Dillan <55008869+timothydillan@users.noreply.github.com>
2021-02-09 10:37:05 +0800

    Update speakers and event schedule

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2021, 01, 26, 19)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2021, 01, 24, 19)}); // year, month, date, hour
         });
 });		
 
